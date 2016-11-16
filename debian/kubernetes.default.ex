014ade985f62cd6064e44cf0ca1a5dedfd7921e2
Alexandros Kosiaris <akosiaris@wikimedia.org>
2016-11-16 14:33:49 +0000

    Initial commit

---
+++ debian/kubernetes.default.ex
@@ -0,0 +1,10 @@
+# Defaults for kubernetes initscript
+# sourced by /etc/init.d/kubernetes
+# installed at /etc/default/kubernetes by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(kubernetes):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="kubernetes" command="/usr/bin/kubernetes"
